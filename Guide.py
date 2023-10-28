a19971f4b8f21c842b6d1fd04465abd16070f293
ray <you@example.com>
2023-10-28 18:50:43 -0400

    first commit

---
+++ Guide.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import numpy as np
+
+st.title("The xG Game")
+
+st.markdown("""
+Expected Goals, xG, is a statistic that has been increasingly visible in football, but what does it mean? In the same way you'd look at an opportunity and say, "He should have score there!", or "That was a touch chance," xG does the same reasoning but attaches a probability to it. The metric bases this probability on historical shot data and a number of factors dependent on who is making the xG model. 
+
+Two factors xG models don't use is the way the player hits the shot, and which part of the goal it is hit to. That's what Post Shot Expected Goals, PSxG, models do, i.e it uses the same factors as its companion xG model plus these two. A notable difference between the two metrics based on their definitions is that off target shots can have an xG of any value, while their PSxG is always 0.
+
+So let's play the xG Game! You have ten shots that ended up in goals, and you have to guess the xG and PSxG of each as a percentage. A perfect score is 0 because your guesses and the actual value would match up perfectly. The theoretical worse score is 1 or 100 in the context of this game. (If you're curious the score being used is the Brier score: https://en.wikipedia.org/wiki/Brier_score.) All data comes from FBREF via Opta:
+
+https://fbref.com/en/matches/3a6836b4/Burnley-Manchester-City-August-11-2023-Premier-League
+
+Scroll all the way down to the "Shots" table to see the values. You could cheat but where's the fun in that?
+
+Go the the Play tab to begin.
+""")
---
+++ pages/1_Play.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import numpy as np
+
+def calc_score(xg_guess, xg, psxg_guess, psxg):
+
+  score = ((xg_guess - xg)**2 + (psxg_guess - psxg)**2)**(1/2)
+
+  return(score)
+  
+counter = 1
+if "counter" not in st.session_state: st.session_state["counter"] = counter
+
+data = {
+1: {"goal": 'https://www.youtube.com/watch?v=qAG6FThzX18', "xg": 14, "psxg": 50},
+2: {"goal": 'https://www.youtube.com/watch?v=FeUNSrVtRgw', "xg": 14, "psxg": 50},
+}
+
+st.title("The xG Game")
+
+st.video(data[1]["goal"])
+
+#col1, col2 = st.columns(2)
+
+xg_guess = st.slider("Guess The xG %", min_value = 0, max_value = 100, value = 50, step = 1)
+psxg_guess = st.slider("Guess The PSxG %", min_value = 0, max_value = 100, value = 50, step = 1)
+submit = st.button("Submit")
+placeholder1 = st.text("""Your score will show here after submission.""")
+#next_goal = st.button("Go To Next Goal")
+
+if submit:
+  with placeholder1.container(): 
+    st.text("""
+    Your xG: {} - Actual xG: {}
+    Your PSxG: {} - Actual PSxG: {}
+    Your Score: {}
+    """.format(xg_guess, data[st.session_state["counter"]]["xg"], psxg_guess, data[st.session_state["counter"]]["psxg"], calc_score(xg_guess, data[st.session_state["counter"]]["xg"], psxg_guess, data[st.session_state["counter"]]["psxg"])))
+    
+#if next_goal:
+#  st.session_state["counter"] = st.session_state["counter"] + 1
+#  print(st.session_state["counter"])
+    
+ 
+
+
